cf78c8f9dd52222379b0e6273ddb47af09d7c31d
panyibo <panyibo@atguigu.com>
2022-09-11 23:20:32 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author pybstart
+ * @create 2022-09-11 23:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
