6081d06132f07e9cd80f17d1a51917e0a9e24d08
panyibo <panyibo@atguigu.com>
2022-09-14 12:38:59 +0800

    gitee commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("fot-fix test!");
         System.out.println("push test!");
         System.out.println("pull test!");
+        System.out.println("gitee test!");
     }
 }
