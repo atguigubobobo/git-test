7200b1b533b4dd2728c11f175ef54f171cbfaa17
panyibo <panyibo@atguigu.com>
2022-10-18 20:37:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Git.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author pybstart
+ * @create 2022-10-18 20:33
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
